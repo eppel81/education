5c5440fff061c86a63c3403dd65aa31a32ed5327
dmitry.gogenko <gogenko@ukr.net>
2015-09-03 17:34:30 +0300

    поправил названия функций

--- Sierpinski_triangle/triangleNew.py
+++ Sierpinski_triangle/triangleNew.py
@@ -12,7 +12,7 @@ import random
 # нужно для постоения фрактала методом хаоса.  
 #
 # initAngle определяет угол левой вершины треульника.  
-def triangleDrawer(leftCornerCoord, sideSize, deep=5):
+def triangle_drawer(leftCornerCoord, sideSize, deep=5):
 	cornersCoord = []
 	initAngle = 60
 
@@ -44,7 +44,7 @@ def triangleDrawer(leftCornerCoord, sideSize, deep=5):
 	return cornersCoord
 
 # Фцнкция передвигает указатель и не оставляет след (не рисует).  
-def movePointer(angle, sideSize):
+def move_pointer(angle, sideSize):
 	"""
 	Передвигает указатель на нужное расстояние и под нужным углом
 	
@@ -60,40 +60,40 @@ def movePointer(angle, sideSize):
 # Далее указатель переезжает на длину стороны в вершину треугольника №1 и 
 # стоится треугольник №2.  Далее указатель переводится на расстояние длины 
 # треугольника с углом -60 градусов в точку вершины треульника №3.  
-def serpTriangleDrawer(leftCornerCoord, sideSize, deep=5):
+def serp_triangle_drawer(leftCornerCoord, sideSize, deep=5):
 	initAngle = 60
 	triagDict = {}
 	
 	if deep > 0:
 		triagDict['firstTriangleCoord'] = leftCornerCoord
 		
-		triangleDrawer(triagDict['firstTriangleCoord'], sideSize, deep)
+		triangle_drawer(triagDict['firstTriangleCoord'], sideSize, deep)
 		
-		movePointer(initAngle,sideSize)
+		move_pointer(initAngle,sideSize)
 		triagDict['secTriangleCoord'] = turtle.pos()
-		triangleDrawer(triagDict['secTriangleCoord'], sideSize, deep)
+		triangle_drawer(triagDict['secTriangleCoord'], sideSize, deep)
 		
-		movePointer(-initAngle,sideSize)
+		move_pointer(-initAngle,sideSize)
 		triagDict['thirdTriangleCoord'] = turtle.pos()
-		triangleDrawer(triagDict['thirdTriangleCoord'], sideSize, deep)
+		triangle_drawer(triagDict['thirdTriangleCoord'], sideSize, deep)
 
 		for key in triagDict:
-			serpTriangleDrawer(triagDict[key], sideSize/2, deep-1)
+			serp_triangle_drawer(triagDict[key], sideSize/2, deep-1)
 
 # Функция поиска середины отрезка по крайним координатам
-def subtrCoord(coord1, coord2):
+def subtr_coord(coord1, coord2):
 	res = (max(coord1, coord2) - min(coord1, coord2))/2
 	return (max(coord1, coord2) - res)
 
 # Функция отображает фрактал методом хаоса. 
-def haosMode(coords=0, numIter=1000):
+def haos_mode(coords=0, numIter=1000):
 	
 	startPoint = (100,75)
 	turtle.dot(2)
 	for i in range(numIter):
 		randCorner = coords[random.randint(0, 2)]
-		newPoint =    (subtrCoord(startPoint[0], randCorner[0]), 
-						subtrCoord(startPoint[1], randCorner[1]))
+		newPoint =    (subtr_coord(startPoint[0], randCorner[0]), 
+						subtr_coord(startPoint[1], randCorner[1]))
 		startPoint = newPoint
 		turtle.penup()
 		turtle.setpos(startPoint[0], startPoint[1])
@@ -117,11 +117,11 @@ def main(alg='iter', coord=(-100,0), sideLength=300, deep=5):
 	countPoints = 3000
 
 	if alg == 'iter':
-		triangleDrawer(coord, sideLength)
-		serpTriangleDrawer(coord, sideLength/2, deep)
+		triangle_drawer(coord, sideLength)
+		serp_triangle_drawer(coord, sideLength/2, deep)
 	elif alg == 'haos':
-		coords = triangleDrawer(coord, sideLength)
-		haosMode(coords , countPoints)
+		coords = triangle_drawer(coord, sideLength)
+		haos_mode(coords , countPoints)
 	else:
 		print 'Задан неправильный алгоритм построения'
 
