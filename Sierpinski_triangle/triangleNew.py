0d80a2f0b569a666d95081dcd5871f9c4ded862a
dmitry.gogenko <gogenko@ukr.net>
2015-09-03 10:24:22 +0300

    Немного дооформил

--- Sierpinski_triangle/triangleNew.py
+++ Sierpinski_triangle/triangleNew.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*- 
 
-import turtle as t
+import turtle
 
-import random as r
+import random
 
 # Функция строит треугольник. Принимает в качестве параметров координаты 
 # левой вершины (кортеж с координатами X Y), длину стороны и текущий уровень
 # вложенности (уровень определяет цвет заливки треугольника)
 #
-# Возвращает все координаты вершин правильного треугольника в списке. Это
-# нужно для постоения фрактала методом хаоса.
+# Возвращает все координаты вершин правильного треугольника в списке.  Это
+# нужно для постоения фрактала методом хаоса.  
 #
-# initAngle определяет угол левой вершины треульника.
-
+# initAngle определяет угол левой вершины треульника.  
 def triangleDrawer(leftCornerCoord, sideSize, deep=5):
 	cornersCoord = []
 	initAngle = 60
@@ -23,48 +22,44 @@ def triangleDrawer(leftCornerCoord, sideSize, deep=5):
 	else:
 		color = 'black'
 
-	t.color('black', color)
-	t.begin_fill()
-	t.penup()
-	t.setpos(leftCornerCoord[0],leftCornerCoord[1])
-	t.pendown()
-	t.setheading(initAngle)
+	turtle.color('black', color)
+	turtle.begin_fill()
+	turtle.penup()
+	turtle.setpos(leftCornerCoord[0],leftCornerCoord[1])
+	turtle.pendown()
+	turtle.setheading(initAngle)
 	cornersCoord.append(leftCornerCoord)
 	
-	t.forward(sideSize)
-	t.right(initAngle*2)
-	cornersCoord.append(t.pos())
+	turtle.forward(sideSize)
+	turtle.right(initAngle*2)
+	cornersCoord.append(turtle.pos())
 	
-	t.forward(sideSize)
-	t.right(initAngle*2)
-	cornersCoord.append(t.pos())
+	turtle.forward(sideSize)
+	turtle.right(initAngle*2)
+	cornersCoord.append(turtle.pos())
 	
-	t.forward(sideSize)
-	t.end_fill()
+	turtle.forward(sideSize)
+	turtle.end_fill()
 
 	return cornersCoord
 
-
-# Фцнкция передвигает указатель и не оставляет след (не рисует)
-
+# Фцнкция передвигает указатель и не оставляет след (не рисует).  
 def movePointer(angle, sideSize):
 	"""
 	Передвигает указатель на нужное расстояние и под нужным углом
 	
 	"""
-	t.setheading(angle)
-	t.penup()
-	t.forward(sideSize)
-	t.pendown()	
+	turtle.setheading(angle)
+	turtle.penup()
+	turtle.forward(sideSize)
+	turtle.pendown()	
 	
-
-# Функция строит три треульника с длиной стороны вдвое меньшей родительского.
-# Расчет позиции левых вершин треугольников исходит из длины стороны и угла.
-# Сначала строит треугольник №1 в левой вершине родительского треугольника. 
+# Функция строит три треульника с длиной стороны вдвое меньшей родительского.  
+# Расчет позиции левых вершин треугольников исходит из длины стороны и угла.  
+# Сначала строит треугольник №1 в левой вершине родительского треугольника.  
 # Далее указатель переезжает на длину стороны в вершину треугольника №1 и 
-# стоится треугольник №2. Далее указатель переводится на расстояние длины 
-# треугольника с углом -60 градусов в точку вершины треульника №3.
-
+# стоится треугольник №2.  Далее указатель переводится на расстояние длины 
+# треугольника с углом -60 градусов в точку вершины треульника №3.  
 def serpTriangleDrawer(leftCornerCoord, sideSize, deep=5):
 	initAngle = 60
 	triagDict = {}
@@ -75,54 +70,48 @@ def serpTriangleDrawer(leftCornerCoord, sideSize, deep=5):
 		triangleDrawer(triagDict['firstTriangleCoord'], sideSize, deep)
 		
 		movePointer(initAngle,sideSize)
-		triagDict['secTriangleCoord'] = t.pos()
+		triagDict['secTriangleCoord'] = turtle.pos()
 		triangleDrawer(triagDict['secTriangleCoord'], sideSize, deep)
 		
 		movePointer(-initAngle,sideSize)
-		triagDict['thirdTriangleCoord'] = t.pos()
+		triagDict['thirdTriangleCoord'] = turtle.pos()
 		triangleDrawer(triagDict['thirdTriangleCoord'], sideSize, deep)
 
 		for key in triagDict:
 			serpTriangleDrawer(triagDict[key], sideSize/2, deep-1)
 
-
 # Функция поиска середины отрезка по крайним координатам
-
 def subtrCoord(coord1, coord2):
 	res = (max(coord1, coord2) - min(coord1, coord2))/2
 	return (max(coord1, coord2) - res)
 
-
 # Функция отображает фрактал методом хаоса. 
-
 def haosMode(coords=0, numIter=1000):
 	
 	startPoint = (100,75)
-	t.dot(2)
+	turtle.dot(2)
 	for i in range(numIter):
-		randCorner = coords[r.randint(0, 2)]
+		randCorner = coords[random.randint(0, 2)]
 		newPoint =    (subtrCoord(startPoint[0], randCorner[0]), 
 						subtrCoord(startPoint[1], randCorner[1]))
 		startPoint = newPoint
-		t.penup()
-		t.setpos(startPoint[0], startPoint[1])
-		t.pendown()
-		t.dot(2)
+		turtle.penup()
+		turtle.setpos(startPoint[0], startPoint[1])
+		turtle.pendown()
+		turtle.dot(2)
 		
-
 
 # Главная функция. Принимает параметры: 
 #     алгоритм (по умолчанию итеративный);
 #     координаты левого угла
 #     длину стороны
 #     глубина разделения в итеративном методе.
-
 def main(alg='iter', coord=(-100,0), sideLength=300, deep=5):
 	"""
 	У функции обязательный параметр alg. Может быть 'iter' или 'haos'
 
 	"""
-	t.speed(10)
+	turtle.speed(10)
 
 	# по умолчанию 3000 точек для нормального проявления
 	countPoints = 3000
@@ -135,7 +124,6 @@ def main(alg='iter', coord=(-100,0), sideLength=300, deep=5):
 		haosMode(coords , countPoints)
 	else:
 		print 'Задан неправильный алгоритм построения'
-
 
 
 if __name__ == '__main__':
